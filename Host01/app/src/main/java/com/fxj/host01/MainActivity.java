aecb3aa5fe1c3513d6fb41931eecba12cf0d1037
fuxianjin <fuxianjin-hj@huajiao.tv>
2018-02-12 19:18:13 +0800

    插件调用宿主中的Activity:宿主Host01增加SecondActivity,供插件Plugin01调用。

--- Host01/app/src/main/java/com/fxj/host01/MainActivity.java
+++ Host01/app/src/main/java/com/fxj/host01/MainActivity.java
@@ -1,27 +1,47 @@
 package com.fxj.host01;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.qihoo360.replugin.RePlugin;
 
 public class MainActivity extends Activity {
 
-    private Button btnStartPlugin01;
+    private String TAG="Host01_MainActivity_fxj0212";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnStartPlugin01=(Button) findViewById(R.id.btn_start_plugin01);
-        btnStartPlugin01.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btn_start_plugin01).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+//                Context plugin01Context=RePlugin.fetchContext("plugin01");
+//                String msg_plugin01Context="获取到的插件plugin01的Context:"+plugin01Context;
+//                Toast.makeText(getApplicationContext(),msg_plugin01Context,Toast.LENGTH_LONG).show();
+//                if(BuildConfig.DEBUG){
+//                    Log.d(TAG,msg_plugin01Context);
+//                }
+
+                /*启动插件的Activity*/
                 RePlugin.startActivity(MainActivity.this, RePlugin.createIntent("plugin01",
                         "com.fxj.replugintest01_plugin01.MainActivity"));
             }
         });
+
+        findViewById(R.id.btn_start_send_activity).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
--- RepluginTest01_Plugin01/app/src/main/java/com/fxj/replugintest01_plugin01/MainActivity.java
+++ RepluginTest01_Plugin01/app/src/main/java/com/fxj/replugintest01_plugin01/MainActivity.java
@@ -1,7 +1,10 @@
 package com.fxj.replugintest01_plugin01;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_start_host01_second_activity).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.setComponent(new ComponentName("com.fxj.host01","com.fxj.host01.SecondActivity"));
+                startActivity(intent);
+            }
+        });
     }
 }
